ddf5b1f69717d23697c581971e61733ec6116521
Qwerty@126 <indrmy.126@gmail.com>
2025-03-12 11:13:49 +0530

    s

--- cloud_react_app/src/components/Hero.jsx
+++ cloud_react_app/src/components/Hero.jsx
@@ -10,8 +10,8 @@ import bgImg from '../assets/cyber-bg.png'
 
 const Hero = () => {
   return (
-    <div name='home' className=' w-full h-screen bg-zinc-200 flex flex-col justify-between'>
-      <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
+    <div name='home' className=' w-full h-screen bg-zinc-200 flex flex-col justify-between py-12'>
+      <div className='grid md:grid-cols-2  max-w-[1240px] m-auto'>
         <div className='flex flex-col justify-center md:items-start w-full px-2 py-8'>
           <p className='text-2xl'>Unique Sequencing & Production</p>
           <h1 className='py-3 text-5xl md:text-7xl font-bold'>Cloud Management</h1>
@@ -21,7 +21,7 @@ const Hero = () => {
         <div>
           <img className='w-full' src={bgImg} alt="/" />
         </div>
-        <div className='absolute flex flex-col py-8 md:min-w-[760px] bottom-[5%] mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl'>
+        <div className='absolute flex flex-col py-4 md:min-w-[760px] bottom-[5%] mx-1 md:left-1/2 transform md:-translate-x-1/2 bg-zinc-200 border border-slate-300 rounded-xl text-center shadow-xl'>
           <p>Data Services</p>
           <div className='flex justify-between flex-wrap'>
             <p className='flex px-4 py-2 text-slate-500'><CloudUploadIcon className='h-6 text-indigo-600'/> App Security</p>
